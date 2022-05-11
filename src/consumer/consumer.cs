ed0931f4909976577b96327f46e52af9080e5c02
Richard Guthrie <rguthriemsft@users.noreply.github.com>
2022-05-11 19:31:20 +0000

    add consumer

--- src/consumer/consumer.cs
+++ src/consumer/consumer.cs
@@ -1,8 +1,10 @@
 ﻿using Confluent.Kafka;
 using System;
+using System.Threading;
 using Microsoft.Extensions.Configuration;
 
-class Producer {
+class Consumer {
+
     static void Main(string[] args)
     {
         if (args.Length != 1) {
@@ -13,37 +15,33 @@ class Producer {
             .AddIniFile(args[0])
             .Build();
 
+        configuration["group.id"] = "kafka-dotnet-getting-started";
+        configuration["auto.offset.reset"] = "earliest";
+
         const string topic = "purchases";
 
-        string[] users = { "eabara", "jsmith", "sgarcia", "jbernard", "htanaka", "awalther" };
-        string[] items = { "book", "alarm clock", "t-shirts", "gift card", "batteries" };
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) => {
+            e.Cancel = true; // prevent the process from terminating.
+            cts.Cancel();
+        };
 
-        using (var producer = new ProducerBuilder<string, string>(
+        using (var consumer = new ConsumerBuilder<string, string>(
             configuration.AsEnumerable()).Build())
         {
-            var numProduced = 0;
-            const int numMessages = 10;
-            for (int i = 0; i < numMessages; ++i)
-            {
-                Random rnd = new Random();
-                var user = users[rnd.Next(users.Length)];
-                var item = items[rnd.Next(items.Length)];
-
-                producer.Produce(topic, new Message<string, string> { Key = user, Value = item },
-                    (deliveryReport) =>
-                    {
-                        if (deliveryReport.Error.Code != ErrorCode.NoError) {
-                            Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
-                        }
-                        else {
-                            Console.WriteLine($"Produced event to topic {topic}: key = {user,-10} value = {item}");
-                            numProduced += 1;
-                        }
-                    });
+            consumer.Subscribe(topic);
+            try {
+                while (true) {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed event from topic {topic} with key {cr.Message.Key,-10} and value {cr.Message.Value}");
+                }
             }
-
-            producer.Flush(TimeSpan.FromSeconds(10));
-            Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            catch (OperationCanceledException) {
+                // Ctrl-C was pressed.
+            }
+            finally{
+                consumer.Close();
+            }
         }
     }
 }
