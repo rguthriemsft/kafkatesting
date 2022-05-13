f9cfd5e85088aa2baeaf0f04977351e8f603390d
Noel Bundick <88900042+noelbundick-msft@users.noreply.github.com>
2022-05-13 20:48:42 +0000

    sample producer/consumer

--- consumer/__main__.py
+++ consumer/__main__.py
@@ -1,2 +1,25 @@
 from confluent_kafka import Consumer
-print("consume events!")
+
+
+c = Consumer(
+    {
+        "bootstrap.servers": "localhost:29092",
+        "group.id": "mygroup",
+        "auto.offset.reset": "earliest",
+    }
+)
+
+c.subscribe(["mytopic"])
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print(f"Consumer error: {msg.error()}")
+        continue
+
+    print(f"Received message: {msg.value().decode('utf-8')}")
+
+c.close()
--- producer/__main__.py
+++ producer/__main__.py
@@ -1,2 +1,28 @@
 from confluent_kafka import Producer
-print("produce events!")
+
+
+p = Producer({"bootstrap.servers": "127.0.0.1:29092"})
+
+
+def delivery_report(err, msg):
+    """Called once for each message produced to indicate delivery result.
+    Triggered by poll() or flush()."""
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+
+
+some_data_source = [str(i) for i in range(1000)]
+for data in some_data_source:
+    # Trigger any available delivery report callbacks from previous produce() calls
+    p.poll(0)
+
+    # Asynchronously produce a message, the delivery report callback
+    # will be triggered from poll() above, or flush() below, when the message has
+    # been successfully delivered or failed permanently.
+    p.produce("mytopic", data.encode("utf-8"), callback=delivery_report)
+
+# Wait for any outstanding messages to be delivered and delivery report
+# callbacks to be triggered.
+p.flush()
