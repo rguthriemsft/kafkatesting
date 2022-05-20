510814ba6942ed0453b391ec76c7341da28ad1a1
mattklup <53016897+mattklup@users.noreply.github.com>
2022-05-20 07:34:34 +0000

    fun with debugging

--- src/python/test/test_e2e.py
+++ src/python/test/test_e2e.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
-import asyncio
+# import asyncio
+import aioredis
 import learningkafka.consumer as consumer  # noqa: E402
 import learningkafka.producer as producer  # noqa: E402
 
 
-def test_simple_e2e_async():
+async def test_simple_kafka_async():
     consume_task = consumer.consume_async()
     message = "hello world"
-    result = asyncio.run(producer.send_one_async(message))
+    result = await producer.send_one_async(message)
     assert result == 1
 
-    result = asyncio.run(consume_task)
+    result = await consume_task
     assert result == bytes(message, "utf8")
+
+
+async def test_simple_redis_async():
+    # Redis client bound to single connection (no auto reconnection).
+    redis = aioredis.from_url("redis://localhost:6379")
+    await redis.set("test", "value")
+    val = await redis.get("test")
+    print(val)
